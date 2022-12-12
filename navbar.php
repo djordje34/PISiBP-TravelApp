f0b62918337ee5841b19c6d50e07f0215336b9e5
Djordje <83914973+djordje34@users.noreply.github.com>
2022-12-12 22:34:59 +0100

    Palette, sample forms, sample navbar controller
    TO DO:
    -logo
    -local images for backgrounds and other frames
    -Backend team to define php post/get methods for forms
    -index.php->dashboard/landing page

---
+++ navbar.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+                <a class="navbar-brand" href="">
+                <img src="" alt="logo" style="width:30px">
+                </a>
+    <?php #$_SESSION['loggedin']
+    if(0){ 
+        echo "
+                <ul class=\"navbar-nav mr-auto mt-2 mt-lg-0\">
+                <li class=\"nav-item active\">
+                    <a class=\"nav-link\" href=\"#\">Home <span class=\"sr-only\">(current)</span></a>
+                </li>
+                <li class=\"nav-item\">
+                    <a class=\"nav-link\" href=\"#\">Link</a>
+                </li>
+                <li class=\"nav-item\">
+                    <a class=\"nav-link disabled\" href=\"#\">Disabled</a>
+                </li>
+                </ul>
+                <form class=\"form-inline my-2 my-lg-0\">
+                <input class=\"form-control mr-sm-2\" type=\"search\" placeholder=\"Search\" aria-label=\"Search\">
+                <button class=\"btn btn-outline-success my-2 my-sm-0\" type=\"submit\">Search</button>
+                </form>
+        ";
+            }?>
+            </div>
+            </nav>
+    </body>
+</html>
