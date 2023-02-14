283f0219116c3201503ad9a358d4a189b2b527a9
mladenkg123 <cvetkovicmladen00@gmail.com>
2023-02-14 23:23:03 +0100

    upd

--- www/index.php
+++ www/index.php
@@ -85,14 +85,14 @@ require_once 'navbar.php';
                             width="28px" height="33px" viewBox="0 0 28 33" enable-background="new 0 0 28 33" xml:space="preserve">
                             <defs>
                                 <linearGradient id='home_grad_prev'>
-                                    <stop offset='0%' stop-color='#fa9e1b'/>
-                                    <stop offset='100%' stop-color='#8d4fff'/>
+                                    <stop offset='0%' stop-color='black'/>
+                                    <stop offset='100%' stop-color='grey'/>
                                 </linearGradient>
                             </defs>
-                            <path class="nav_path" fill="#F3F6F9" d="M19,0H9C4.029,0,0,4.029,0,9v15c0,4.971,4.029,9,9,9h10c4.97,0,9-4.029,9-9V9C28,4.029,23.97,0,19,0z
+                            <path class="nav_path" fill="White" d="M19,0H9C4.029,0,0,4.029,0,9v15c0,4.971,4.029,9,9,9h10c4.97,0,9-4.029,9-9V9C28,4.029,23.97,0,19,0z
                             M26,23.091C26,27.459,22.545,31,18.285,31H9.714C5.454,31,2,27.459,2,23.091V9.909C2,5.541,5.454,2,9.714,2h8.571
                             C22.545,2,26,5.541,26,9.909V23.091z"/>
-                            <polygon class="nav_arrow" fill="#F3F6F9" points="15.044,22.222 16.377,20.888 12.374,16.885 16.377,12.882 15.044,11.55 9.708,16.885 11.04,18.219 
+                            <polygon class="nav_arrow" fill="White" points="15.044,22.222 16.377,20.888 12.374,16.885 16.377,12.882 15.044,11.55 9.708,16.885 11.04,18.219 
                             11.042,18.219 "/>
                         </svg>
                     </div>
@@ -103,14 +103,14 @@ require_once 'navbar.php';
                         width="28px" height="33px" viewBox="0 0 28 33" enable-background="new 0 0 28 33" xml:space="preserve">
                             <defs>
                                 <linearGradient id='home_grad_next'>
-                                    <stop offset='0%' stop-color='#fa9e1b'/>
-                                    <stop offset='100%' stop-color='#8d4fff'/>
+                                    <stop offset='0%' stop-color='black'/>
+                                    <stop offset='100%' stop-color='grey'/>
                                 </linearGradient>
                             </defs>
-                        <path class="nav_path" fill="#F3F6F9" d="M19,0H9C4.029,0,0,4.029,0,9v15c0,4.971,4.029,9,9,9h10c4.97,0,9-4.029,9-9V9C28,4.029,23.97,0,19,0z
+                        <path class="nav_path" fill="White" d="M19,0H9C4.029,0,0,4.029,0,9v15c0,4.971,4.029,9,9,9h10c4.97,0,9-4.029,9-9V9C28,4.029,23.97,0,19,0z
                         M26,23.091C26,27.459,22.545,31,18.285,31H9.714C5.454,31,2,27.459,2,23.091V9.909C2,5.541,5.454,2,9.714,2h8.571
                         C22.545,2,26,5.541,26,9.909V23.091z"/>
-                        <polygon class="nav_arrow" fill="#F3F6F9" points="13.044,11.551 11.71,12.885 15.714,16.888 11.71,20.891 13.044,22.224 18.379,16.888 17.048,15.554 
+                        <polygon class="nav_arrow" fill="White" points="13.044,11.551 11.71,12.885 15.714,16.888 11.71,20.891 13.044,22.224 18.379,16.888 17.048,15.554 
                         17.046,15.554 "/>
                         </svg>
                     </div>
